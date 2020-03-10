5fec50e79faa5c530049d014ee0825e2254e7903
Hafizjavaid <hafizjaidi786@gmail.com>
2020-03-10 18:46:17 +0500

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ window.addEventListener("scroll", () => {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+var toggle = document.querySelector(".toggle");
+var toggle2 = document.querySelectorAll(".act");
+
+toggle.addEventListener("click", () => {
+  var header = document.querySelector("header");
+  header.classList.toggle("activee");
+  console.log(toggle2);
+});
+
+toggle2.forEach(item => {
+  item.addEventListener("click", () => {
+    var header = document.querySelector("header");
+    header.classList.toggle("activee");
+  });
+});
